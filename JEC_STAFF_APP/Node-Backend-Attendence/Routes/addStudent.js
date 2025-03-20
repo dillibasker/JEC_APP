dfed513d85727394c5fb3328f1773235e651b5fd
Dilli Basker <dillibasker1@gmail.com>
2025-03-20 18:27:39 +0530

    First Commit

--- JEC_STAFF_APP/Node-Backend-Attendence/Routes/addStudent.js
+++ JEC_STAFF_APP/Node-Backend-Attendence/Routes/addStudent.js
@@ -2,9 +2,9 @@ const express=require("express")
 const router=express.router()
 const Student=require("../../Models/students")
 
-// Add a sudent  logic 
+// Add a student  logic 
 
-router.post("/students",async(req,res)=>{
+router.post("/addStudents",async(req,res)=>{
     const student=new Student(req.body)
     await student.save();
     res.status(201).send(student)
---
+++ JEC_STAFF_APP/Node-Backend-Attendence/Routes/getAllStudents.js
@@ -0,0 +1,11 @@
+const express=require("express")
+const router=express.router()
+const Student=require("../../Models/students")
+
+//get all student - department wise
+
+router.get("/students/:department/:year/:section",async(req,res)=>{
+    const {department, year, section}=req.params
+    const students=await Student.find(department,year,section)
+    res.send(students)
+})
