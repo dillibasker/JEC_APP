1d54fa5aadf150170e92726f132421d8aad957a7
Dilli Basker <dillibasker1@gmail.com>
2025-03-20 18:34:11 +0530

    First Commit

--- JEC_STAFF_APP/Node-Backend-Attendence/Routes/Attendence/markAttendence.js
+++ JEC_STAFF_APP/Node-Backend-Attendence/Routes/Attendence/markAttendence.js
@@ -6,4 +6,7 @@ router.post("/attendence",async(req,res)=>{
     const { rollNumber, date, status } = req.body;
     const student = await Student.findOne({ rollNumber });
 if (!student) return res.status(400).send("Student Not found") 
+    student.attendence.push({date,status})
+    await student.save();
+    res.send(student);
 })
